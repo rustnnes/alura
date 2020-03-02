38d1ac270a222a08c1cf43c0c21c2094cc22dd80
Diego Rustnnes Bandeira <diego.rustnnes@conductor.com.br>
2020-03-02 02:15:48 -0300

    Rollback para Hibernate 4.x, para poder executar os testes
    TODO: adaptar codebase para Hibernate 5.x

--- src/main/java/br/com/caelum/pm73/dao/CriadorDeSessao.java
+++ src/main/java/br/com/caelum/pm73/dao/CriadorDeSessao.java
@@ -11,30 +11,30 @@ import org.hibernate.cfg.Configuration;
 @SuppressWarnings("deprecation")
 public class CriadorDeSessao {
 
-	private static AnnotationConfiguration config;
-	private static SessionFactory sf;
-	
-	public Session getSession() {
-		if(sf == null) {
-			sf = getConfig().buildSessionFactory();
-		}
-		
-		return sf.openSession();
-	}
+    private static AnnotationConfiguration config;
+    private static SessionFactory sf;
 
-	public Configuration getConfig() {
-		if(config == null) {
-			config = new AnnotationConfiguration()
-		    .addAnnotatedClass(Lance.class)
-		    .addAnnotatedClass(Leilao.class)
-		    .addAnnotatedClass(Usuario.class)
-			.setProperty("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver")
-			.setProperty("hibernate.connection.url", "jdbc:hsqldb:caelum.db;shutdown=true")
-			.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect")
-			.setProperty("hibernate.connection.username", "sa")
-			.setProperty("hibernate.connection.password", "")
-			.setProperty("hibernate.show_sql", "true");
-		}
-		return config;
-	}
+    public static Session getSession() {
+        if (sf == null) {
+            sf = getConfig().buildSessionFactory();
+        }
+
+        return sf.openSession();
+    }
+
+    public static Configuration getConfig() {
+        if (config == null) {
+            config = new AnnotationConfiguration()
+                    .addAnnotatedClass(Lance.class)
+                    .addAnnotatedClass(Leilao.class)
+                    .addAnnotatedClass(Usuario.class)
+                    .setProperty("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver")
+                    .setProperty("hibernate.connection.url", "jdbc:hsqldb:caelum.db;shutdown=true")
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect")
+                    .setProperty("hibernate.connection.username", "sa")
+                    .setProperty("hibernate.connection.password", "")
+                    .setProperty("hibernate.show_sql", "true");
+        }
+        return config;
+    }
 }
